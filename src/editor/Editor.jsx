6f1c6826c99502e8ab310d4ff305d2666025f79c
Marl Tumulak <marl.tumulak@aereus.com>
2015-10-13 23:47:57 +0800

    Finished most of the features including view source

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -6,7 +6,7 @@ var TextFieldRich = require("../TextFieldRich.jsx");
 var FontIcon = require("../FontIcon.jsx");
 var IconButton = require("../IconButton.jsx");
 var DropDownIcon = require("../DropDownIcon.jsx");
-
+var Dialog = require("../Dialog.jsx");
 
 var fontStyleOptions = [
     { payload: 'p', text: 'Body' },
@@ -15,6 +15,15 @@ var fontStyleOptions = [
     { payload: 'h3', text: 'Heading 3' },
     { payload: 'h4', text: 'Heading 4' }
 ];
+
+var fontOptions = [
+                        { payload: 'p', text: 'Arial' },
+                        { payload: 'h1', text: 'Georgia' },
+                        { payload: 'h2', text: 'Tahoma' },
+                        { payload: 'h3', text: 'Courier New' },
+                        { payload: 'h4', text: 'Times New Roman' },
+                        { payload: 'h4', text: 'Verdana' }
+                    ];
 
 var Editor = React.createClass({
 
@@ -25,40 +34,60 @@ var Editor = React.createClass({
     },
 
     render: function() {
+    	
         return (
             <div>
                 <Toolbar>
                     <ToolbarGroup key={1} float="left">
-                        <FontIcon onClick={this._handleBoldClick} className="cfi cfi-bold" />
-                        <FontIcon className="cfi cfi-italic" />
-                        <FontIcon className="cfi cfi-underline" />
+                        <FontIcon onClick={this._handleClick.bind(this, "bold")} className="cfi cfi-bold" />
+                        <FontIcon onClick={this._handleClick.bind(this, "italic")} className="cfi cfi-italic" />
+                        <FontIcon onClick={this._handleClick.bind(this, "underline")} className="cfi cfi-underline" />
                     </ToolbarGroup>
                     <ToolbarGroup key={2} float="left">
-                        <FontIcon className="cfi cfi-align-left" />
-                        <FontIcon className="cfi cfi-align-center" />
-                        <FontIcon className="cfi cfi-align-right" />
+                        <FontIcon onClick={this._handleClick.bind(this, "justifyleft")} className="cfi cfi-align-left" />
+                        <FontIcon onClick={this._handleClick.bind(this, "justifycenter")} className="cfi cfi-align-center" />
+                        <FontIcon onClick={this._handleClick.bind(this, "justifyright")} className="cfi cfi-align-right" />
                     </ToolbarGroup>
-                    <ToolbarGroup key={3} float="right">
+                    <ToolbarGroup key={3} float="left">
+                    	<FontIcon onClick={this._handleClick.bind(this, "src")} className="cfi cfi-file-text-o" />
+                    	<FontIcon onClick={this._handleClick.bind(this, "inserthorizontalrule")} className="cfi cfi-minus" />
+                    	<FontIcon onClick={this._handleClick.bind(this, "link")} className="cfi cfi-link" />
+                    	<FontIcon onClick={this._handleClick.bind(this, "table")} className="cfi cfi-table" />
+                    </ToolbarGroup>
+                    <ToolbarGroup key={4} float="right">
                         <DropDownIcon iconClassName="cfi cfi-font" menuItems={fontStyleOptions} />
                     </ToolbarGroup>
                 </Toolbar>
                 <div>
                     <TextFieldRich ref="rte" />
                 </div>
+                <Dialog title="test" />
             </div>
         )
     },
 
     /**
-     * Callback used to handle commands when bold is clicked on the toolbar
+     * Callback used to handle commands when button is clicked on the toolbar
      *
-     * @param {string} cmd The name of the command to execute
+     * @param {string} type		The name of the command to execute
      * @private
      */
-    _handleBoldClick: function(evt) {
-        this.refs.rte.sendCommand("bold");
+    _handleClick: function(type) {
+    	switch(type) {
+    		case "src":
+    			this.refs.rte._toggleSrc();
+    			break;
+    		case "link":
+    			this.refs.rte._promptDialog();	
+    			break;
+    		case "table":
+    			this.refs.rte._insertHtml('<table style="border: 1px solid; padding: 10px;"><tbody><tr><td>&nbsp;</td><td>&nbsp;</td></tr></tbody></table>');
+    			break;
+    		default:
+    			this.refs.rte._sendCommand(type, '');
+    			break;
+    	}
     },
-
 });
 
 module.exports = Editor;
