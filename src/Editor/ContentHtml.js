3ba8e373b2a53523d7c5d3d2bc84780183a173f1
marl.tumulak <marl.tumulak@aereus.com>
2016-10-26 21:02:20 +0800

    Updated the editor to use the material ui fonts.

---
+++ src/Editor/ContentHtml.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ThemeService from '../styles/ChamelThemeService';
+import RichText from '../Input/RichText';
+import EditorToolbar from './EditorToolbar';
+
+/**
+ * Contains both the toolbar and an instance of RichText component
+ */
+class ContentHtml extends React.Component {
+
+    /**
+     * Set accepted properties
+     */
+    static propTypes = {
+        /**
+         * The callback function used when user changes the content of the editor
+         *
+         * @type {function}
+         */
+        onChange: React.PropTypes.func,
+
+        /**
+         * The callback function used when user looses the focus of the editor
+         *
+         * @type {function}
+         */
+        onBlur: React.PropTypes.func,
+
+        /**
+         * The initial value of the content editor
+         *
+         * @type {string}
+         */
+        value: React.PropTypes.string,
+
+        /**
+         * Handles the toggling of content view
+         *
+         * @type {function}
+         */
+        onContentViewToggle: React.PropTypes.func
+    }
+
+    /**
+     * An alternate theme may be passed down by a provider
+     */
+    static contextTypes = {
+        chamelTheme: React.PropTypes.object
+    };
+
+    /**
+     * Class constructor
+     *
+     * @param {Object} props Properties to send to the render function
+     */
+    constructor(props) {
+
+        // Call parent constructor
+        super(props);
+
+        this.state = {
+            value: this.props.value,
+            toggleStyle: null,
+            toggleType: null
+        };
+    }
+
+    /**
+     * Handle when the editor has changed
+     *
+     * @param {string} value The value of the editor
+     * @private
+     */
+    _onChange = (value) => {
+        if (this.props.onChange) {
+            this.props.onChange(value);
+        }
+
+        this.setState({value});
+    }
+
+    _onToggle = (value) => {
+        if (this.props.onChange) {
+            this.props.onChange(value);
+        }
+
+        this.setState({
+            value,
+            toggleStyle: null,
+            toggleType: null
+        });
+    }
+
+    /**
+     * Handle when the editor looses the focus
+     *
+     * @param {string} value The value of the editor
+     * @private
+     */
+    _onBlur = (value) => {
+        if (this.props.onBlur) {
+            this.props.onBlur(value);
+        } else {
+            //this.setState({value});
+        }
+    }
+
+    /**
+     * Handles when the user set the focus in the editor
+     *
+     * @param {string} value The value of the editor
+     * @private
+     */
+    _onFocus = (value) => {
+        if (this.props.onFocus) {
+            this.props.onFocus(value);
+        } else {
+            //this.setState({value});
+        }
+    }
+
+    /**
+     * Handles the toggling of styles in the toolbar icons
+     *
+     * @param {string} style The style that was clicked. It is either block style or inline style
+     * @param {string} type The style type that was toggled. (e.g. bold, italic, underline, h1, h2, h3, ul, ol)
+     * @private
+     */
+    _handleStyleToggle = (style, type) => {
+        console.log('toggle');
+        this.setState({
+            toggleStyle: style,
+            toggleType: type
+        })
+    }
+
+    /**
+     * Handles the toggling of content view
+     *
+     * @param {int} contentView The content view we are switching to
+     * @private
+     */
+    _handleContentViewToggle = (contentView) => {
+        if (this.props.onContentViewToggle) {
+            this.props.onContentViewToggle(contentView, this.state.value);
+        }
+    }
+
+    render() {
+
+        // Determine which theme to use
+        let theme = (this.context.chamelTheme && this.context.chamelTheme.editor)
+            ? this.context.chamelTheme.editor : ThemeService.defaultTheme.editor;
+
+        return (
+            <div>
+                <EditorToolbar
+                    contentViewType={this.props.contentViewType}
+                    onStyleToggle={this._handleStyleToggle}
+                    onContentViewToggle={this._handleContentViewToggle}
+                />
+                <RichText
+                    commandToggleType={this.state.toggleType}
+                    commandToggleStyle={this.state.toggleStyle}
+                    onToggle={this._onToggle}
+                    onChange={this._onChange}
+                    onBlur={this._onBlur}
+                    onFocus={this._onFocus}
+                    value={this.state.value}
+                />
+            </div>
+        );
+    }
+}
+
+export default ContentHtml;
---
+++ src/Editor/EditorToolbar.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Toolbar from 'chamel/Toolbar/Toolbar';
+import ToolbarGroup from 'chamel/Toolbar/ToolbarGroup';
+import IconButton from 'chamel/Button/IconButton';
+import TitleIcon from 'chamel/icons/font/TitleIcon';
+import ListBulletedIcon from 'chamel/icons/font/ListBulletedIcon';
+import ListNumberedIcon from 'chamel/icons/font/ListNumberedIcon';
+import BoldIcon from 'chamel/icons/font/BoldIcon';
+import ItalicIcon from 'chamel/icons/font/ItalicIcon';
+import UnderlinedIcon from 'chamel/icons/font/UnderlinedIcon';
+import CodeIcon from 'chamel/icons/font/CodeIcon';
+import WebIcon from 'chamel/icons/font/WebIcon';
+
+// Block Types that are used in the editor and displayed in the toolbar
+const BLOCK_TYPES = [
+    {label: 'H1', style: 'header-one', class: TitleIcon, props: {headerType: 1}},
+    {label: 'H2', style: 'header-two', class: TitleIcon, props: {headerType: 2}},
+    {label: 'H3', style: 'header-three', class: TitleIcon, props: {headerType: 3}},
+    {label: 'H4', style: 'header-four', class: TitleIcon, props: {headerType: 4}},
+    {label: 'H5', style: 'header-five', class: TitleIcon, props: {headerType: 5}},
+    {label: 'H6', style: 'header-six', class: TitleIcon, props: {headerType: 6}},
+    {label: 'OL', style: 'ordered-list-item', class: ListBulletedIcon, props: {}},
+    {label: 'UL', style: 'unordered-list-item', class: ListNumberedIcon, props: {}}
+];
+
+// Inline Styles that are used in the editor and displayed in the toolbar
+const INLINE_STYLES = [
+    {label: 'Bold', style: 'BOLD', class: BoldIcon, props: {}},
+    {label: 'Italic', style: 'ITALIC', class: ItalicIcon, props: {}},
+    {label: 'Underline', style: 'UNDERLINE', class: UnderlinedIcon, props: {}}
+];
+
+// Types of content views
+const HTML_VIEW = 'html';
+const SOURCE_VIEW = 'source';
+
+/**
+ * Handle when displaying the block type controls and
+ * Set the styles of the controls if it is toggled or not
+ */
+class EditorToolbar extends React.Component {
+    static propTypes = {
+
+        /**
+         * Determines which toolbar should we display for current type of content view
+         *
+         * @var {int}
+         */
+        contentViewType: React.PropTypes.oneOf([HTML_VIEW, SOURCE_VIEW]),
+
+        /**
+         * The callback function used when user toggles the styles in the toolbar buttons
+         *
+         * @var {func}
+         */
+        onStyleToggle: React.PropTypes.func,
+
+        /**
+         * The callback function used when user toggles content view of the editor
+         *
+         * @var {func}
+         */
+        onContentViewToggle: React.PropTypes.func
+    }
+
+    /**
+     * Class constructor
+     *
+     * @param {Object} props Properties to send to the render function
+     */
+    constructor(props) {
+
+        // Call parent constructor
+        super(props);
+    }
+
+    render() {
+
+        let displayBlockStyles = [];
+        let displayInlineStyles = [];
+        let displayConteViewType = [];
+
+        switch (this.props.contentViewType) {
+            case HTML_VIEW:
+
+                // Loop thru the block types and setup the style button
+                BLOCK_TYPES.map((blockType, idx) => {
+
+                    displayBlockStyles.push(
+                        <IconButton key={idx} onTap={(e) => { this.props.onStyleToggle("block", blockType.style); }}>
+                            <blockType.class
+                                {...blockType.props}
+                            />
+                        </IconButton>
+                    );
+                });
+
+                // Loop thru the inline styles and setup the style button
+                INLINE_STYLES.map((inlineType, idx) => {
+
+                    displayInlineStyles.push(
+                        <IconButton key={idx} onTap={(e) => { this.props.onStyleToggle("inline", inlineType.style); }}>
+                            <inlineType.class
+                                {...inlineType.props}
+                            />
+                        </IconButton>
+                    );
+                });
+
+                displayConteViewType = (
+                    <IconButton onTap={(e) => { this.props.onContentViewToggle(SOURCE_VIEW); }}>
+                        <CodeIcon />
+                    </IconButton>
+                )
+                break;
+
+            case SOURCE_VIEW:
+                displayConteViewType = (
+                    <IconButton onTap={(e) => { this.props.onContentViewToggle(HTML_VIEW); }}>
+                        <WebIcon />
+                    </IconButton>
+                )
+                break;
+        }
+
+        return (
+            <div>
+                <Toolbar>
+                    <ToolbarGroup key={0} align="left">
+                        {displayBlockStyles}
+                    </ToolbarGroup>
+                    <ToolbarGroup key={1} align="right">
+                        {displayConteViewType}
+                    </ToolbarGroup>
+                </Toolbar>
+                <Toolbar secondary>
+                    <ToolbarGroup align="left">
+                        {displayInlineStyles}
+                    </ToolbarGroup>
+                </Toolbar>
+            </div>
+        );
+    }
+}
+
+export default EditorToolbar;
