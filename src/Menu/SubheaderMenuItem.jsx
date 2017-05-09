77183f11944e7dd47c89df1b277ecdbe9070a17a
root <you@example.com>
2017-05-09 21:02:01 +0800

    Update the way of calling the ThemeService

--- src/Menu/SubheaderMenuItem.jsx
+++ src/Menu/SubheaderMenuItem.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ThemeService from '../styles/ChamelThemeService';
 
-const SubheaderMenuItem = (props) => {
-  let theme = ThemeService.defaultTheme.menu;
+const SubheaderMenuItem = (props, context) => {
+  const theme = (context.chamelTheme && context.chamelTheme.menu)
+    ? context.chamelTheme.menu : ThemeService.defaultTheme.menu;
+
   return (
     <div key={props.index} className={theme.menuSubheader}>{props.text}</div>
   );
