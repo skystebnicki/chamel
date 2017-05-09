d5b87eb12fa9dd9dda93dfc1640520c3bded4bec
root <you@example.com>
2017-05-09 20:42:58 +0800

    Applied the classname like the rest of the components

--- src/Menu/SubheaderMenuItem.jsx
+++ src/Menu/SubheaderMenuItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ThemeService from '../styles/ChamelThemeService';
 
 const SubheaderMenuItem = (props) => {
+  let theme = ThemeService.defaultTheme.menu;
   return (
-    <div key={props.index} className="chamel-subheader">{props.text}</div>
+    <div key={props.index} className={theme.menuSubheader}>{props.text}</div>
   );
 }
 
