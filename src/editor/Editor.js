e231f3c1b4ff2752a37a57552fa92376796ec034
marl.tumulak <marl.tumulak@aereus.com>
2017-05-10 23:30:29 +0800

    Increased package.json version and Updated Editor.js

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import ThemeService from '../styles/ChamelThemeService';
