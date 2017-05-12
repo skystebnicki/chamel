fe79551d51f04525fb02cb8b6be1e05e2c755c57
marl.tumulak <marl.tumulak@aereus.com>
2017-05-13 02:13:42 +0800

    Fixed the classNames

--- src/DatePicker/CalendarMonth.jsx
+++ src/DatePicker/CalendarMonth.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import DateTime from '../utils/DateTime';
 import DayButton from './DayButton';
+import classNames from 'classnames';
 
-const _getDayElements = (week, props) => {
-  let displayDayElements = [];
-  week.forEach((day, i) => {
-    const selected = DateTime.isEqualDate(props.selectedDate, day);
+class CalendarMonth extends Component {
 
-    displayDayElements.push(
-      <DayButton
-        key={i}
-        date={day}
-        disabled={(day) => {
-          const minDate = props.minDate;
-          const maxDate = props.maxDate;
+  render() {
+    const classes = this.getClasses('chamel-date-picker-calendar-month-week');
+
+    const weekArray = DateTime.getWeekArray(this.props.displayDate);
+    let displayWeekElements = [];
+
+    weekArray.forEach((week, i) => {
+      displayWeekElements.push(
+        <div
+          key={i}
+          className={classes}>
+          {this._getDayElements(week)}
+        </div>
+      );
+    });
+
+    return (
+      <div className={"chamel-date-picker-calendar-month"}>
+        {displayWeekElements}
+      </div>
+    );
+  };
+
+  _getDayElements = (week) => {
+    let displayDayElements = [];
+    week.forEach((day, i) => {
+      const selected = DateTime.isEqualDate(this.props.selectedDate, day);
+
+      displayDayElements.push(
+        <DayButton
+          key={i}
+          date={day}
+          disabled={(day) => {
+          const minDate = this.props.minDate;
+          const maxDate = this.props.maxDate;
 
           if (minDate != null && day < minDate) {
             return true;
@@ -26,36 +52,47 @@ const _getDayElements = (week, props) => {
 
           return false;
         }}
-        onClick={(e, date) => {
-          if (props.onDayTouchTap) props.onDayTouchTap(e, date);
+          onClick={(e, date) => {
+          if (this.props.onDayTouchTap) this.props.onDayTouchTap(e, date);
         }}
-        selected={selected}/>
-    );
-  });
+          selected={selected}/>
+      );
+    });
 
-  return displayDayElements;
-};
+    return displayDayElements;
+  };
 
-const CalendarMonth = (props) => {
+  getClasses = (initialClasses, additionalClassObj) => {
+    let classString = '';
 
-  const weekArray = DateTime.getWeekArray(props.displayDate);
-  let displayWeekElements = [];
+    //Initialize the classString with the classNames that were passed in
+    if (this.this.props.className) classString += ' ' + this.this.props.className;
 
-  weekArray.forEach((week, i) => {
-    displayWeekElements.push(
-      <div
-        key={i}
-        className="chamel-date-picker-calendar-month-week">
-        {_getDayElements(week, props)}
-      </div>
-    );
-  });
+    //Add in initial classes
+    if (typeof initialClasses === 'object') {
+      classString += ' ' + classNames(initialClasses);
+    } else {
+      classString += ' ' + initialClasses;
+    }
 
-  return (
-    <div className={"chamel-date-picker-calendar-month"}>
-      {displayWeekElements}
-    </div>
-  );
+    //Add in additional classes
+    if (additionalClassObj) classString += ' ' + classNames(additionalClassObj);
+
+    //Convert the class string into an object and run it through the class set
+    return classNames(this.getClassSet(classString));
+  };
+
+  getClassSet = (classString) => {
+    let classObj = {};
+
+    if (classString) {
+      classString.split(' ').forEach(function (className) {
+        if (className) classObj[className] = true;
+      });
+    }
+
+    return classObj;
+  };
 }
 
 CalendarMonth.propTypes = {
